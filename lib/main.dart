3994a5ca23725cee94f4228f3e1cbc76bb9403cb
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-23 18:02:58 +0900

    위젯 생성

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          color: Colors.amber,
+        ),
       ),
     );
   }
