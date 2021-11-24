6fca45bf6d423a3384bb4e62de5913b43d85b12e
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-24 17:56:18 +0900

    text 위젯 button 위젯

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -1,5 +1,6 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_clone/utils/logger.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({Key? key}) : super(key: key);
@@ -10,6 +11,23 @@ class IntroPage extends StatelessWidget {
       children: [
         Text('브로콤비'),
         ExtendedImage.asset('assets/images/brocombi_intro.png'),
+        Text('우리 동네 중고 직거래 브로콤비'),
+        Text('브로콤비는 동네 직거래 마켓이에요.'),
+        Text('내 동네를 설정하고 시작해보세요!'),
+        TextButton(
+          onPressed: () {
+            logger.d('on text button clicked!!!');
+          },
+          child: Text(
+            '내 동네 설정하고 시작하기',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.blue,
+          ),
+        ),
       ],
     );
   }
