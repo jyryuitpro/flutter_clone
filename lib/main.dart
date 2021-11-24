04008ed26747a69b73c82471beef08c38c6168cd
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-24 10:11:06 +0900

    파일 나누기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,44 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset('assets/images/brocombi.png'),
-            Container(
-              height: 100,
-              color: Colors.green,
-            ),
-            Container(
-              height: 100,
-              color: Colors.yellow,
-            ),
-            Container(
-              height: 100,
-              color: Colors.amber,
-            ),
-            Container(
-              height: 100,
-              color: Colors.green,
-            ),
-            Container(
-              height: 100,
-              color: Colors.yellow,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
