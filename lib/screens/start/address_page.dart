19c4a4b9762ab6598790050990e3e0bc943cfcb5
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-25 11:44:06 +0900

    ListView padding & TextButton padding 컨트롤

--- lib/screens/start/address_page.dart
+++ lib/screens/start/address_page.dart
@@ -9,8 +9,9 @@ class AddressPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      minimum: EdgeInsets.all(16),
+      minimum: EdgeInsets.only(left: 16, right: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           TextFormField(
             decoration: const InputDecoration(
@@ -31,28 +32,25 @@ class AddressPage extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  CupertinoIcons.compass,
-                  color: Colors.white,
-                  size: 20,
-                ),
-                label: Text(
-                  '현재위치로 찾기',
-                  style: Theme.of(context).textTheme.button,
-                ),
-                style: TextButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-              ),
-            ],
+          TextButton.icon(
+            onPressed: () {},
+            icon: Icon(
+              CupertinoIcons.compass,
+              color: Colors.white,
+              size: 20,
+            ),
+            label: Text(
+              '현재위치로 찾기',
+              style: Theme.of(context).textTheme.button,
+            ),
+            style: TextButton.styleFrom(
+              backgroundColor: Theme.of(context).primaryColor,
+              minimumSize: Size(10, 40),
+            ),
           ),
           Expanded(
             child: ListView.builder(
+              padding: EdgeInsets.symmetric(vertical: 16),
               itemCount: 30,
               itemBuilder: (context, index) {
                 // logger.d('index: $index');
