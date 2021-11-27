bddaca6a60d08004d2906385267407196fa50268
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-27 23:13:57 +0900

    Rounded Corner

--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -8,61 +8,94 @@ class ItemsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: EdgeInsets.all(common_padding),
-      separatorBuilder: (context, index) {
-        return Divider(
-          height: (common_padding * 2) + 1,
-          thickness: 1,
-          color: Colors.grey[300],
-          indent: common_small_padding,
-          endIndent: common_small_padding,
-        );
-      },
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return SizedBox(
-          height: 100,
-          child: Row(
-            children: [
-              ExtendedImage.network('https://picsum.photos/100'),
-              SizedBox(
-                width: common_padding,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('brocombi', style: Theme.of(context).textTheme.subtitle1,),
-                    Text('1일전', style: Theme.of(context).textTheme.subtitle2,),
-                    Text('5,000원'),
-                    Expanded(
-                      child: Container(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        Size size = MediaQuery.of(context).size;
+        final imgSize = size.width / 4;
+
+        return ListView.separated(
+          padding: EdgeInsets.all(common_padding),
+          separatorBuilder: (context, index) {
+            return Divider(
+              height: (common_padding * 2) + 1,
+              thickness: 1,
+              color: Colors.grey[300],
+              indent: common_small_padding,
+              endIndent: common_small_padding,
+            );
+          },
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return SizedBox(
+              height: imgSize,
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: imgSize,
+                    width: imgSize,
+                    child: ExtendedImage.network(
+                      'https://picsum.photos/100',
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
+                  ),
+                  SizedBox(
+                    width: common_padding,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          height: 14,
-                          child: FittedBox(
-                            fit: BoxFit.fitHeight,
-                            child: Row(
-                              children: [
-                                Icon(CupertinoIcons.chat_bubble_2, color: Colors.grey,),
-                                Text('23', style: TextStyle(color: Colors.grey),),
-                                Icon(CupertinoIcons.heart, color: Colors.grey,),
-                                Text('30', style: TextStyle(color: Colors.grey),),
-                              ],
+                        Text(
+                          'brocombi',
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                        Text(
+                          '1일전',
+                          style: Theme.of(context).textTheme.subtitle2,
+                        ),
+                        Text('5,000원'),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              height: 14,
+                              child: FittedBox(
+                                fit: BoxFit.fitHeight,
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      CupertinoIcons.chat_bubble_2,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      '23',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    Icon(
+                                      CupertinoIcons.heart,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      '30',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
